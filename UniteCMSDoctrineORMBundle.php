df439168dd56ff403fa146c8a4e64641fd7d7035
Franz Wilding <franz@unite.co.at>
2019-11-08 17:34:46 +0100

    Start to implement unite admin bundle as a vue SPA

--- UniteCMSDoctrineORMBundle.php
+++ UniteCMSDoctrineORMBundle.php
@@ -3,5 +3,8 @@
 namespace UniteCMS\DoctrineORMBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use UniteCMS\CoreBundle\UniteCMSCoreBundle;
 
-class UniteCMSDoctrineORMBundle extends Bundle {}
+class UniteCMSDoctrineORMBundle extends Bundle {
+    const UNITE_VERSION = UniteCMSCoreBundle::UNITE_VERSION;
+}
